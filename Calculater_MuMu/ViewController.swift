b82393a060743d3996ac3f06f709a5a656a7791e
gai lowvapong <mumu@gais-mac.local>
2017-01-22 19:14:10 -0800

    Initial Commit

---
+++ Calculater_MuMu/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Calculater_MuMu
+//
+//  Created by gai lowvapong on 1/22/17.
+//  Copyright © 2017 gai lowvapong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
