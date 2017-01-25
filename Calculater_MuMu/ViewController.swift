ea316c4452e8c7432e3de0a03252b288afdb6018
gai lowvapong <mumu@gais-mac.local>
2017-01-25 02:06:37 -0800

    dsfsdfsd

--- Calculater_MuMu/ViewController.swift
+++ Calculater_MuMu/ViewController.swift
@@ -20,6 +20,45 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBOutlet weak var input1_txt: UITextField!
+    @IBOutlet weak var input2_txt: UITextField!
+    @IBOutlet weak var result_txt: UILabel!
+    
+    @IBOutlet weak var plus_btn: UIButton!
 
+    @IBOutlet weak var minus_btn: UIButton!
+    @IBOutlet weak var mult_btn: UIButton!
+    @IBOutlet weak var div_btn: UIButton!
+
+    @IBAction func Onplus_enter(_ sender: Any) {
+        let a:Double! = Double(input1_txt.text!)
+        let b:Double! = Double(input2_txt.text!)
+        
+        result_txt.text = String(a+b)
+        
+    }
+    
+    @IBAction func Onminus_enter(_ sender: Any) {
+        let a:Double! = Double(input1_txt.text!)
+        let b:Double! = Double(input2_txt.text!)
+        
+        result_txt.text = String(a-b)
+    }
+
+    @IBAction func Onmult_enter(_ sender: Any) {
+        let a:Double! = Double(input1_txt.text!)
+        let b:Double! = Double(input2_txt.text!)
+        
+        result_txt.text = String(a*b)
+    }
+    
+    @IBAction func Ondiv_enter(_ sender: Any) {
+        let a:Double! = Double(input1_txt.text!)
+        let b:Double! = Double(input2_txt.text!)
+        
+        result_txt.text = String(a/b)
+    }
+    
+    
 }
 
